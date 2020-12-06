0d5fd49482556026865a0a530e37d181fbb138ad
Slava <vsvoiteshonok1@gmail.com>
2020-12-06 23:31:06 +0300

    done

---
+++ 2Lab/2Lab/ETL.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _2Lab
+{
+    public partial class ETL : ServiceBase
+    {
+        Logger logger;
+        string source = "D:\\" + "Source\\";
+        string target = "D:\\" + "Target\\";
+        public ETL()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Directory.CreateDirectory(source);
+            Directory.CreateDirectory(target);
+
+            logger = new Logger(source, target);
+            Thread loggerThread = new Thread(new ThreadStart(logger.Start));
+            loggerThread.Start();
+        }
+
+        protected override void OnStop()
+        {
+            logger.Stop();
+            try
+            {
+                Directory.Delete(source, true);
+            }
+            catch { }
+            try
+            {
+                Directory.Delete(target, true);
+            }
+            catch { }
+        }
+    }
+}
---
+++ 2Lab/2Lab/Logger.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _2Lab
+{
+    class Logger
+    {
+        FileSystemWatcher watcher;
+        bool enabled = true;
+        string source;
+        string target;
+        public Logger(string source, string target)
+        {
+            this.source = source;
+            this.target = target;
+            watcher = new FileSystemWatcher(source);
+            watcher.Created += Created;
+        }
+
+        public void Start()
+        {
+            watcher.EnableRaisingEvents = true;
+            while (enabled)
+            {
+                Thread.Sleep(100);
+            }
+        }
+        public void Stop()
+        {
+            watcher.EnableRaisingEvents = false;
+            enabled = false;
+        }
+        /// <summary>
+        /// adding files
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Created(object sender, FileSystemEventArgs e)
+        {
+            string time = DateTime.Now.ToString("dd/MM/yyyy/hh//mm//ss");
+            Encryptor.Crypt(e.FullPath);
+            Archivator.Compress(e.FullPath, $"{target}{time}.txt.gz");
+            Archivator.Decompress($"{target}{time}.txt.gz");
+            Encryptor.Decrypt($"{target}{time}.txt");
+        }
+    }
+}
